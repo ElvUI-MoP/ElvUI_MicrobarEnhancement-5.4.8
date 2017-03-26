c42caf116e539419afcd01fd69d7be16e6361514
Appolyonn <jellefenxd@gmail.com>
2017-03-26 02:59:31 +0200

    ElvUI_MicrobarEnhancement

---
+++ ElvUI_MicrobarEnhancement/locales/english.lua
@@ -0,0 +1,9 @@
+﻿-- English localization file for enGB.
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
+local L = AceLocale:NewLocale("ElvUI", "enUS", true);
+if not L then return; end
+
+L["Set Scale"] = "Set Scale"
+L["Sets Scale of the Micro Bar"] = "Sets Scale of the Micro Bar"
+L["As Letters"] = true
+L["Replace icons with letters"] = true
---
+++ ElvUI_MicrobarEnhancement/locales/russian.lua
@@ -0,0 +1,9 @@
+﻿--Файл локализации для ruRU
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
+local L = AceLocale:NewLocale("ElvUI", "ruRU")
+if not L then return; end
+
+L["Set Scale"] = "Масштаб"
+L["Sets Scale of the Micro Bar"] = "Устанавливает масштаб микроменю"
+L["As Letters"] = "В виде букв"
+L["Replace icons with letters"] = "Заменить иконки буквами"
